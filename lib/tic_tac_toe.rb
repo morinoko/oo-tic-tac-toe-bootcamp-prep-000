8ad5e7c6329c6e70430676f128d3e3020befc770
Felice Forby <feliceforby@gmail.com>
2018-02-08 23:10:25 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -110,7 +110,7 @@ class TicTacToe
   
   # Check if the board is full, but there are no winners
   def draw?
-    won? && full?
+    !won? && full?
   end
   
   # Check if game is over (won, full board, or a draw)
